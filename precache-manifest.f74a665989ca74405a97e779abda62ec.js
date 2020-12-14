bc6e334bed78f60ffec15a9532ace7b2bd0e6e7b
SKarakayaa <sefakarakayaa@gmail.com>
2020-12-14 20:15:41 +0300

    Updates

---
+++ precache-manifest.f74a665989ca74405a97e779abda62ec.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d18299974c5f6f7cafb6ed194adfc754",
+    "url": "/myrestaurant/index.html"
+  },
+  {
+    "revision": "ee72c91850e655167c3d",
+    "url": "/myrestaurant/static/css/2.dc51db0e.chunk.css"
+  },
+  {
+    "revision": "2755a44caad0fa66975a",
+    "url": "/myrestaurant/static/css/main.dfc1ac2e.chunk.css"
+  },
+  {
+    "revision": "ee72c91850e655167c3d",
+    "url": "/myrestaurant/static/js/2.49811538.chunk.js"
+  },
+  {
+    "revision": "da09adea01b9f40deb02df1af3bef772",
+    "url": "/myrestaurant/static/js/2.49811538.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2755a44caad0fa66975a",
+    "url": "/myrestaurant/static/js/main.25c4ed85.chunk.js"
+  },
+  {
+    "revision": "58d177ae348f52ffba7d",
+    "url": "/myrestaurant/static/js/runtime-main.e4c4d0d7.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/myrestaurant/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/myrestaurant/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/myrestaurant/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/myrestaurant/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/myrestaurant/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
