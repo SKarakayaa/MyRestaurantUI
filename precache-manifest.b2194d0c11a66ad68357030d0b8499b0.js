097dce8b53daecd10b37df2507d2b17a2c0a30d3
SKarakayaa <sefakarakayaa@gmail.com>
2020-12-15 21:23:41 +0300

    Updates

---
+++ precache-manifest.b2194d0c11a66ad68357030d0b8499b0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "501c5adf977efcc73a2e608389cdfac2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd3d4a9e16337549b1b8",
+    "url": "/static/css/2.4449a171.chunk.css"
+  },
+  {
+    "revision": "1e8072b170a2e98c2107",
+    "url": "/static/css/main.dfc1ac2e.chunk.css"
+  },
+  {
+    "revision": "dd3d4a9e16337549b1b8",
+    "url": "/static/js/2.03d0c7e6.chunk.js"
+  },
+  {
+    "revision": "da09adea01b9f40deb02df1af3bef772",
+    "url": "/static/js/2.03d0c7e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e8072b170a2e98c2107",
+    "url": "/static/js/main.c9ba2de2.chunk.js"
+  },
+  {
+    "revision": "da93d8a2b69cb7508b2a",
+    "url": "/static/js/runtime-main.b627569f.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/myrestaurant/precache-manifest.f74a665989ca74405a97e779abda62ec.js"
+  "/precache-manifest.b2194d0c11a66ad68357030d0b8499b0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/myrestaurant/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
